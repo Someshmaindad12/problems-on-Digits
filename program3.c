508041d5d30825456f4408e7626a3d5b78ce7d7a
Someshwar Maindad <124545422+Someshmaindad12@users.noreply.github.com>
2025-02-10 18:51:34 +0530

    Add files via upload

---
+++ program3.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int CountDigits(int iNo)
+{
+    int iCount = 0 ; 
+    int iDigit = 0 ; 
+    if(iNo < 0 )
+    {
+        iNo = -iNo;
+    }
+    while(iNo > 0 )
+    {
+        
+        iCount++;
+        iNo = iNo /10; 
+    }
+    return iCount;
+}
+int main()
+{
+    int iValue = 0 ; 
+    int iRet = 0 ; 
+    printf("Enter Number : ");
+    scanf("%d",&iValue);
+    iRet = CountDigits(iValue);
+    printf("%d",iRet);
+    return 0 ;
+}
